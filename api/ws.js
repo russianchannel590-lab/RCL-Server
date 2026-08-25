818976c88de964beb5fb6ae94d9bf7aa7b41fa81
russianchannel590-lab <russianchannel590@gmail.com>
2026-08-25 21:33:22 +0500

    Update ws.js

--- api/ws.js
+++ api/ws.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { createServer } from 'node:http';
 import { WebSocketServer } from 'ws';
 
+import { setupRclWebSocket } from '../server/rclWebSocket.js';
+
 const app = express();
 
 app.get('/', (_req, res) => {
@@ -13,35 +15,11 @@ app.get('/', (_req, res) => {
 
 const server = createServer(app);
 
-const wss = new WebSocketServer({ server });
-
-wss.on('connection', (ws) => {
-  console.log('RCL client connected');
-
-  ws.send(JSON.stringify({
-    type: 'connected',
-    message: 'RCL WebSocket connection established'
-  }));
-
-  ws.on('message', (data) => {
-    const message = data.toString();
-
-    console.log('Received:', message);
-
-    // Пока просто возвращаем сообщение обратно.
-    ws.send(JSON.stringify({
-      type: 'echo',
-      message
-    }));
-  });
-
-  ws.on('close', () => {
-    console.log('RCL client disconnected');
-  });
-
-  ws.on('error', (error) => {
-    console.error('WebSocket error:', error);
-  });
+const wss = new WebSocketServer({
+  server,
+  maxPayload: 1024
 });
+
+setupRclWebSocket(wss);
 
 export default server;
